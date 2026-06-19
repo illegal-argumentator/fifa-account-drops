d76f29ef457a9099857e9d33a892b41f8a9aae98
HaaaamBurger <gemasclashes@gmail.com>
2026-06-19 12:24:47 +0300

    user decomposition impl

--- src/main/java/com/daniel/fifa_account_drops/adapter/account/in/internal/AccountDropsStarter.java
+++ src/main/java/com/daniel/fifa_account_drops/adapter/account/in/internal/AccountDropsStarter.java
@@ -5,12 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
-@Profile("server")
 @RequiredArgsConstructor
 final class AccountDropsStarter implements ApplicationRunner {
 
